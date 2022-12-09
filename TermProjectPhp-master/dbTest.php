4b41a540275777970104b81e4b04f4df205d55cb
Yanky597 <64791155+Yanky597@users.noreply.github.com>
2022-12-09 10:46:48 -0500

    Update dbTest.php

--- TermProjectPhp-master/dbTest.php
+++ TermProjectPhp-master/dbTest.php
@@ -6,29 +6,15 @@ require('config/db.php');
 
 $username = mysqli_real_escape_string($link, $_GET['username']);
 
-// sets up a query from the database
-//$query = 'SELECT * username FROM AuthorizedUsers WHERE username = "chana123"';
-
-
 $query = 'SELECT username FROM AuthorizedUsers WHERE username = "chana123" AND pass = "1234"';
 
 // the result of the query is stored in this variable
 $result = mysqli_query($link, $query);
 
-//fetch data and stores it in an associative array
-//$users = mysqli_fetch_assoc($result); -> fetches a single user
-
-/* this fetches all the users from the database */
-//$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
 if($result){
     $count = mysqli_num_rows($result);
     echo $count;
 }
-
-//prints the result of the query 
-
-//var_dump($users);
 
 // frees up the data
 mysqli_free_result($result);
